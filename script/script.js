1ce69ff4aeaa5a93372b2589669df3ed4a4d256d
Dardallin <dardallin@gmail.com>
2019-09-02 18:56:24 +0500

    First I deleted all plugins. Added setting for gallery. It's working!

--- script/script.js
+++ script/script.js
@@ -1,19 +1,9 @@
-$(document).ready(function() {
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
+$('.popup-gallery').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+        delegate: 'a', // the selector for gallery item
         type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
         gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function(item) {
-                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-            }
+            enabled:true
         }
     });
 });
